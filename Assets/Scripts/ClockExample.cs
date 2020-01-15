5b44d71a1655e6168d2dc33f57feae2c2f737134
Vl1dimir <vovan9900@yandex.ru>
2020-01-16 01:06:08 +0300

    2

---
+++ Assets/Scripts/ClockExample.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+public class ClockExample : MonoBehaviour
+{
+    private Text textClock; void Awake()
+    {
+        textClock = GetComponent<Text>();
+    }
+    void Update()
+    {
+        DateTime time = DateTime.Now;
+        string hour = LeadingZero(time.Hour);
+        string minute = LeadingZero(time.Minute);
+        string second = LeadingZero(time.Second); textClock.text = hour + ":" + minute;
+    }
+    string LeadingZero(int n)
+    {
+        return n.ToString().PadLeft(2, '0');
+    }
+}
---
+++ Assets/Scripts/SoundWaitting.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundWaitting : MonoBehaviour {
+	public AudioSource _AudioSource;
+	public AudioClip Sound;
+	public bool loop;
+	public float Timer;
+	private float TimerDown;
+	// Use this for initialization
+	void Start () {
+		TimerDown = Timer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (TimerDown > 0)
+			TimerDown -= Time.deltaTime; 
+		if (TimerDown < 0)
+			TimerDown = 0; 
+		if (TimerDown == 0) {
+			_AudioSource.PlayOneShot(Sound);
+			if(loop)
+			{
+			TimerDown = Timer;
+			}
+			else
+			{
+				gameObject.GetComponent<SoundWaitting>().enabled = false;
+			 }
+		}
+	}
+}

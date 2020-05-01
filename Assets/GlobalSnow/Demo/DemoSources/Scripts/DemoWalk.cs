3f399d6d5c5cbed8d1c159546c87f693afa52dd7
DESKTOP-CCB0JI2\Vladimir <vovan9900@yandex.ru>
2020-05-01 06:14:27 +0300

    WIP

---
+++ Assets/GlobalSnow/Demo/DemoSources/Scripts/DemoWalk.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace GlobalSnowEffect {
+    public class DemoWalk : MonoBehaviour {
+        GlobalSnow snow;
+
+        void Start() {
+            snow = GlobalSnow.instance;
+        }
+
+        void Update() {
+            if (Input.GetKeyDown(KeyCode.T)) {
+                snow.enabled = !snow.enabled;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                GlobalSnow.instance.MarkSnowAt(Camera.main.transform.position, 3f);
+
+
+            }
+
+        }
+    }
+}
